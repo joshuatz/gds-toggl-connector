836b9da1b591ec7859c8fae70f706d0a0f2d9604
Joshua Tzucker <joshua.tz@gmail.com>
2019-05-30 12:25:31 -0700

    manifest

--- main.js
+++ main.js
@@ -26,15 +26,40 @@ var togglApiInst = new TogglApi(getUserApiKey());
  * @override
  */
 function getConfig() {
-
 	// Get config obj provided by gds-connector
 	var config = cc.getConfig();
 
 	// Set config general info
 	config.newInfo()
 		.setId('instructions')
-		.setText('Configure the connector for Toggl');
+        .setText('Configure the connector for Toggl');
+    
+    config.setDateRangeRequired(true);
 
 	// Make sure to return the built config
 	return config.build();
+}
+
+/**
+ * @override
+ */
+function getFields(){
+    var fields = cc.getFields();
+    var types = cc.FieldType;
+    var aggregations = cc.AggregationType;
+
+    fields
+        .newDimension()
+        .setId('day')
+        .setName('Date')
+        .setType(types.YEAR_MONTH_DAY);
+
+    return fields;
+}
+
+/**
+ * @override
+ */
+function getSchema(request) {
+    return {schema : getFields().build()};
 }

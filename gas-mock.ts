ad22c7cd300a3bae97e70c17670d1d3dd57fe656
Joshua Tzucker <joshua.tz@gmail.com>
2019-07-18 16:36:42 -0700

    working on mocking DataStudioApp

--- gas-mock.ts
+++ gas-mock.ts
@@ -9,7 +9,6 @@
 var DataStudioApp:GoogleAppsScript.Data_Studio.DataStudioApp = {
     createCommunityConnector: function(): GoogleAppsScript.Data_Studio.CommunityConnector {
         return {
-            FieldType: {"YEAR":0,"YEAR_QUARTER":1,"YEAR_MONTH":2,"YEAR_WEEK":3,"YEAR_MONTH_DAY":4,"YEAR_MONTH_DAY_HOUR":5,"QUARTER":6,"MONTH":7,"WEEK":8,"MONTH_DAY":9,"DAY_OF_WEEK":10,"DAY":11,"HOUR":12,"MINUTE":13,"DURATION":14,"COUNTRY":15,"COUNTRY_CODE":16,"CONTINENT":17,"CONTINENT_CODE":18,"SUB_CONTINENT":19,"SUB_CONTINENT_CODE":20,"REGION":21,"REGION_CODE":22,"CITY":23,"CITY_CODE":24,"METRO":25,"METRO_CODE":26,"LATITUDE_LONGITUDE":27,"NUMBER":28,"PERCENT":29,"TEXT":30,"BOOLEAN":31,"URL":32,"HYPERLINK":33,"IMAGE":34,"IMAGE_LINK":35,"CURRENCY_AED":36,"CURRENCY_ALL":37,"CURRENCY_ARS":38,"CURRENCY_AUD":39,"CURRENCY_BDT":40,"CURRENCY_BGN":41,"CURRENCY_BOB":42,"CURRENCY_BRL":43,"CURRENCY_CAD":44,"CURRENCY_CDF":45,"CURRENCY_CHF":46,"CURRENCY_CLP":47,"CURRENCY_CNY":48,"CURRENCY_COP":49,"CURRENCY_CRC":50,"CURRENCY_CZK":51,"CURRENCY_DKK":52,"CURRENCY_DOP":53,"CURRENCY_EGP":54,"CURRENCY_ETB":55,"CURRENCY_EUR":56,"CURRENCY_GBP":57,"CURRENCY_HKD":58,"CURRENCY_HRK":59,"CURRENCY_HUF":60,"CURRENCY_IDR":61,"CURRENCY_ILS":62,"CURRENCY_INR":63,"CURRENCY_IRR":64,"CURRENCY_ISK":65,"CURRENCY_JMD":66,"CURRENCY_JPY":67,"CURRENCY_KRW":68,"CURRENCY_LKR":69,"CURRENCY_LTL":70,"CURRENCY_MNT":71,"CURRENCY_MVR":72,"CURRENCY_MXN":73,"CURRENCY_MYR":74,"CURRENCY_NOK":75,"CURRENCY_NZD":76,"CURRENCY_PAB":77,"CURRENCY_PEN":78,"CURRENCY_PHP":79,"CURRENCY_PKR":80,"CURRENCY_PLN":81,"CURRENCY_RON":82,"CURRENCY_RSD":83,"CURRENCY_RUB":84,"CURRENCY_SAR":85,"CURRENCY_SEK":86,"CURRENCY_SGD":87,"CURRENCY_THB":88,"CURRENCY_TRY":89,"CURRENCY_TWD":90,"CURRENCY_TZS":91,"CURRENCY_UAH":92,"CURRENCY_USD":93,"CURRENCY_UYU":94,"CURRENCY_VEF":95,"CURRENCY_VND":96,"CURRENCY_YER":97,"CURRENCY_ZAR":98},
             AggregationType: {
                 AVG: 0,
                 COUNT: 1,
@@ -20,12 +19,61 @@ var DataStudioApp:GoogleAppsScript.Data_Studio.DataStudioApp = {
                 AUTO: 6,
                 NO_AGGREGATION: 7
             },
+            AuthType: {
+                "NONE" : 0,
+                "OAUTH2": 1,
+                "USER_PASS": 2,
+                "KEY": 3,
+                "USER_TOKEN": 4
+            },
+            BigQueryParameterType: GoogleAppsScript.Data_Studio.BigQueryParameterType,
+            FieldType: GoogleAppsScript.Data_Studio.FieldType,
             getConfig: function(): GoogleAppsScript.Data_Studio.Config {
-                build: function(): any {
-                    //
-                },
-                newCheckbox: function(): GoogleAppsScript.Data_Studio.Checkbox {
-                    //
+                let _this = this;
+                return {
+                    build: function(): object {
+                        return {};
+                    },
+                    newCheckbox: function(): GoogleAppsScript.Data_Studio.Checkbox {
+                        return {
+                            setAllowOverride: function(allowOverride: boolean): GoogleAppsScript.Data_Studio.Checkbox {
+                                return this;
+                            },
+                            setHelpText: function(helpText: string): GoogleAppsScript.Data_Studio.Checkbox{
+                                return this;
+                            },
+                            setId: function(id:string): GoogleAppsScript.Data_Studio.Checkbox {
+                                return this;
+                            },
+                            setName: function(name:string): GoogleAppsScript.Data_Studio.Checkbox {
+                                return this;
+                            }
+                        }
+                    },
+                    newInfo: function(): GoogleAppsScript.Data_Studio.Info {
+                        //
+                    },
+                    newOptionBuilder: function(): GoogleAppsScript.Data_Studio.OptionBuilder {
+                        //
+                    },
+                    newSelectMultiple: function(): GoogleAppsScript.Data_Studio.SelectMultiple {
+                        //
+                    },
+                    newSelectSingle: function(): GoogleAppsScript.Data_Studio.SelectSingle {
+                        //
+                    },
+                    newTextArea: function(): GoogleAppsScript.Data_Studio.TextArea {
+                        //
+                    },
+                    newTextInput: function(): GoogleAppsScript.Data_Studio.TextInput {
+                        //
+                    },
+                    printJson: function(): string {
+                        //
+                    },
+                    setDateRangeRequired: function(dateRangeRequired:boolean): GoogleAppsScript.Data_Studio.Config {
+                        return _this;
+                    }
                 }
             },
             getFields: function(): GoogleAppsScript.Data_Studio.Fields {
@@ -52,18 +100,37 @@ var DataStudioApp:GoogleAppsScript.Data_Studio.DataStudioApp = {
                 },
                 newMetric: function(): GoogleAppsScript.Data_Studio.Field {
                     //
+                },
+                setDefaultDimension: function(fieldId: string): void {
+                    //
+                },
+                setDefaultMetric: function(fieldId: string): void {
+                    //
                 }
-            }
-        }
-    }}
-}
-PropertiesService = {
-    getUserProperties : function(){
-        return {
-            getProperty: function(input){
-                return 'asfasfasdf';
+            },
+            newAuthTypeResponse: function(): GoogleAppsScript.Data_Studio.GetAuthTypeResponse {
+                //
+            },
+            newBigQueryConfig: function(): GoogleAppsScript.Data_Studio.BigQueryConfig {
+                //
+            },
+            newDebugError: function(): GoogleAppsScript.Data_Studio.DebugError {
+                //
+            },
+            newUserError: function(): GoogleAppsScript.Data_Studio.UserError {
+                //
             }
         }
     }
 }
+
+// PropertiesService = {
+//     getUserProperties : function(){
+//         return {
+//             getProperty: function(input){
+//                 return 'asfasfasdf';
+//             }
+//         }
+//     }
+// }
 

fe8cd553ac1abacafcc6e1893d80385bd417c17a
Joshua Tzucker <joshua.tz@gmail.com>
2019-07-18 05:01:26 -0700

    Added jest, jest types, and ts-jest for testing. Added a few quick tests to make sure it is functional

---
+++ gas-mock.js
@@ -0,0 +1,6 @@
+/**
+ * @author Joshua Tzucker
+ * @file gas-mock.js
+ * Attempts to mock / shim / polyfill GAS environment stuff (like UrlFetchApp)
+ * @TODO build out, if necessary for tests (or delete if not)
+ */
